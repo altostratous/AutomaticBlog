80f0f56aaa6fa2b838c1d6a4796ec5c3d4891fff
altostratous <altostratous@gmail.com>
2016-08-27 12:16:01 +0430

    Added while statement and fixed blogfa.

---
+++ FrontEndAutomation/XulFxWhileStatement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FrontEndAutomation
+{
+    public class XulFxWhileStatement : Statement
+    {
+        public XulFxWhileStatement(string name, XmlNode xmlContent)
+        {
+            Name = name;
+            Condition = xmlContent["Condition"].InnerText;
+            Do = Compiler.Compile(xmlContent["Do"].FirstChild);
+        }
+        public string Name { get; set; }
+        public string Condition { get; set; }
+        public Statement Do { get; set; }
+        
+        public object Process(Executor executor)
+        {
+            while (true)
+            {
+                try {
+                    if (!executor.Execute(Condition).ToString().Equals("True"))
+                        break;
+                }catch(Exception ex)
+                {
+                }
+                Do.Process(executor);
+                System.Threading.Thread.Sleep(Common.WAIT_INTERVAL);
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Gecko.Xpcom.DoEvents(true);
+                    Gecko.Xpcom.DoEvents();
+                    System.Windows.Forms.Application.DoEvents();
+                }
+            }
+            return null;
+        }
+    }
+}
